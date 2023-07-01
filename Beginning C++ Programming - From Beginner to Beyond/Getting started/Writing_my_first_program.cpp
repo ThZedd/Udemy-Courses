68122b0924a2ba20561eeae4d24a1950ad556eeb
Bernardo Carvalho <122268749+ThZedd@users.noreply.github.com>
2023-07-01 14:00:33 +0100

    Add the files from the 1 section of the course

---
+++ Beginning C++ Programming - From Beginner to Beyond/Getting started/Writing_my_first_program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int favorite_number;
+
+  cout << "Enter your favorite number between 1 and 100:";
+
+  cin >> favorite_number;
+  
+  cout << "Amazing!! That´s my favorite number too" << std::endl;
+
+  return 0;
+
+}
---
+++ Beginning C++ Programming - From Beginner to Beyond/Getting started/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+    int favorite_number;
+    cout << favorite_number << endl;
+    return 0;
+}
